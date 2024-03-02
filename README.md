a3038600b2bf7e432860a94bd78f56b91ec00da9
Priyash Sharma <146582441+PriyAsh-24@users.noreply.github.com>
2024-03-02 14:02:21 +0530

    Update till Lecture 4 Without Food app

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # React Learning Notes 
 
+## To get parcel and Get React
+- npm init
+- npm isntall -d parcel . -d ka mtlb hai dev dependencies chaiye
+- npm install react
+- npm install react-dom
+
+
 ## Parcel
 - Dev Build
 - local Host
@@ -18,4 +25,84 @@
 - Tree Shaking - remove unused code
 - Diffrent dev and prod bundles
     1. to create dev files we removed main from package 
-    <img title="a title" alt="Alt text" src="Screenshot 2024-02-28 205017.png">
+    ![Notes](Screenshot 2024-02-28 205017.png)
+
+## Browser List
+[It will tell you about the values to add in package.json .it is added as a key pair ](https://browserslist.dev/?q=bGFzdCAyIHZlcnNpb25z)
+
+
+# Lecture 3
+
+-we can use script command for npm shotcuts for example
+```javascript
+"scripts": {
+    "test": "jest",
+    "start" : "parcel  index.html"  
+  },
+```
+-now we can just write npm run start .There is also a shortcut only for start we can write npm start.
+
+-JSX code mai notes hai 
+
+-react components
+
+# Lecture 4
+
+- css in js
+
+### props in js
+  -they are using function arguments type 
+
+### Config-driven UI
+  - acoding to diffrent data from backend you can show different ui
+  - for example swiggy will have diffrent coupens aur corousals (which show offers) based on the data they get from server in diffrent cities
+
+## Data from swiggy(key is imp)
+  -map 
+    -key : maanlo bohot saara data hai agar key nahi hogi toh har baar react uss data ko render karega 100 baar hai toh 100 baar render karega isliye apan key de dete hai jab bhi koi naya data aaya toh woh apni key ke hisab se arrange ho jaaye
+    -also never use array index as your keys
+    [reason by react.org](https://robinpokorny.com/blog/index-as-a-key-is-an-anti-pattern/)
+
+## React Fragment(Assignment)
+  -React Fragments are a component in React that allows you to group a list of children without adding extra nodes to the DOM. This can be useful when rendering multiple child elements/components in a single parent component.
+  ```javascript
+    function Parent() {
+      return (
+        <>
+          <Child1 />
+          <Child2 />
+        </>
+      );
+    }
+  ```
+
+## Virtual DOM
+-The VDOM is a programming concept that keeps a virtual representation of a user interface (UI) in memory. It's a tree that represents the structure and content of a website
+
+## React Reconciliation
+- React reconciliation is the process of comparing the current state of the UI (virtual DOM) with the desired state (new virtual DOM) and figuring out the most efficient way to update the real DOM to match the desired state. This process is what makes React so fast and efficient, as it avoids unnecessary DOM updates.
+
+## React Fiber
+- React Fiber works by breaking down the rendering process into smaller chunks, called fibers. Fibers are units of work that can be scheduled and executed independently. This allows React to prioritize and render the most important parts of the UI first, while keeping the rest of the UI responsive.
+
+## React without JSX
+[Reason](https://legacy.reactjs.org/docs/react-without-jsx.html)
+
+
+# Lecture 5
+
+- Two Type of Export / Import
+
+  1. Default export/import
+    ```javascript
+    export default Component_name
+    import  Component_name from 'module'
+    ```
+
+  2. Named Export Import
+    ```javascript
+    export const component_name
+    import {component_name} from 'module'
+    ```
+
+
--- app.js
+++ app.js
@@ -1,16 +1,112 @@
-const heading =React.createElement('h1',{ id:'heading' , xyz:'abc'},'Hello world from React')  //React.createElement(tag,attribute,child);
-// console.log(heading);
+import React from "react"
+import ReactDOM from  'react-dom/client'
 
-const root=ReactDOM.createRoot(document.getElementById("root"));
+// const heading =React.createElement('h1',{ id:'heading' , xyz:'abc'},'Hello world from React')  //React.createElement(tag,attribute,child);
+// // console.log(heading);
+
+// const root=ReactDOM.createRoot(document.getElementById("root"));
 // root.render(heading);
 
 
-//Lets Create a Nested loop
+// //Lets Create a Nested loop
 
-const nested =React.createElement('div',{id: 'parent'},
-    React.createElement('div',{id:'child'},
-    [React.createElement('h1',{},'Hey! its H1'),React.createElement('h2',{},'Hey its H2')]
-))
+// const nested =React.createElement('div',{id: 'parent'},
+//     React.createElement('div',{id:'child'},
+//     [React.createElement('h1',{},'Hey! its H1 did it change'),React.createElement('h2',{},'Hey its H2 did it changess here')]
+// ))
 
-root.render(nested);
+// root.render(nested);
+
+
+//React.createlement creates an object which while rendering converts to a html code
+//* createElement => ReactElement(object) => render
+const heading = React.createElement('h1',{id : "hello"},'Hello World');
+
+//THis is a long code to write and is not easily undeerstood by other devlopers
+//This is the place where jsx comes in \
+//  * jSX = it is an extenstion to our reac code which made it easier to write 
+//it is not html intoduced in js 
+
+
+
+//* jsx => React.createElement => ReactElement(object) => render      (babel Transpiles this code)
+const jsxHeading=<h1 id="hello">This is Hello World From jsx</h1>
+//this line is not read by js compiler (ecma script 6) and so here parcel come in
+//parcel converts this code which is readable using Babel
+
+//console.log(heading);
+//console.log(jsxHeading) //gives the same result  //i. e. is a object
+
+
+const root=ReactDOM.createRoot(document.querySelector('#root'));
+
+root.render(jsxHeading)
+
+
+
+//for long jsx code we write
+
+const jsxheading=(
+<h1 className="hello">
+    This is Hello World From jsx  with classname 
+</h1>);
+
+
+
+//* React Componets
+// Class Based Component -old
+// Functional Component -New 
+
+//React FUnctional Component   --Always starts with capital letter
+
+const HeadingComponent=() =>{
+    return <h1 className="Heading">Hello World using Functional Component </h1>
+}
+
+const HeadingComponent2=() => (
+    <div className="Constainer">
+        <h1 className="Heading">Hello World using Functional Component </h1>
+    </div>
+);
+
+//() isme return nahi likhna 
+//{} isme return liknaa hai js chai with code mai padha tha
+
+root.render(<HeadingComponent/>);
+
+
+//* Component Composition
+
+const ToInject=() => <h1 className="toinject">This is My heading</h1>
+
+const MainHeading=() => (
+    <div className="Container">
+        <ToInject />                       
+        {
+        //  ^ this is done by basel
+        }
+        <h1>Hello Inside</h1>
+    </div>
+)
+
+root.render(<MainHeading/>)
+
+//we can also insert js inside html using {}
+
+const number=100;
+
+const InsertingJs=() => (
+    <div className="Container">
+        <h1>
+        {
+            //we can Insert Any js inside
+            number +100
+        }
+        {console.log("jaa na")}
+        </h1>
+        <h1>Hello Inside</h1>
+    </div>
+)
+
+root.render(<InsertingJs/>)
 
